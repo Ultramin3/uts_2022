1c0a5a33553c69ee31f2e386cbb82f28888dd002
Ultramin3 <113913316+Ultramin3@users.noreply.github.com>
2022-09-19 22:01:58 +0700

    Add files via upload

---
+++ Aurell_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Aurellya sabrina ps" <<endl;
+    cout << "umur: 15 Tahun" <<endl;
+}    
+    
---
+++ aurellya_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Aurellya sabrina ps"  <<endl;
+     cout << "Kelas: x rpl 2" <<endl;
+      cout << "Nilai: 100" <<endl;
+}
